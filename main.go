305274aaf771b4f4abed664db2b5b1765d18251c
Thanh Nhan <99491940+LNThanhNhan@users.noreply.github.com>
2023-12-29 14:29:13 +0700

    Support docker

--- main.go
+++ main.go
@@ -8,19 +8,29 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const JSON_FILE = "./serviceAccountKey.json"
+
 func main() {
+	fmt.Println("Server is running...")
 	router := mux.NewRouter()
 	LoadFileEnv()
 	imageRouter := router.PathPrefix("/img").Subrouter()
 	imageRouter.HandleFunc("/create", UploadImage).Methods("POST")   // Request need a image file from form-data
 	imageRouter.HandleFunc("/update", UpdateImage).Methods("PUT")    // Request need a image file from form-data and id of image
 	imageRouter.HandleFunc("/delete", DeleteImage).Methods("DELETE") // Request need id of image
-	http.ListenAndServe("127.0.0.1:8080", router)
+	http.ListenAndServe("0.0.0.0:8090", router)
 }
 
 func LoadFileEnv() {
-	err := godotenv.Load()
+	err := godotenv.Load("./.env")
 	if err != nil {
-		fmt.Print("Error loading .env file")
+		fmt.Println("Error loading .env file")
+		fmt.Println(err.Error())
 	}
+}
+
+func HandleError(w http.ResponseWriter, err error, msg string) {
+	errMsg := msg + ": " + err.Error()
+	jsonData := MakeFailResponse(errMsg)
+	ReturnResponse(w, jsonData)
 }
